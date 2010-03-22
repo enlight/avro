fa06d55fd1a59e3fc2a8fa74c41a2a91f8edd500
Bruce Mitchener <bruce.mitchener@gmail.com>
2010-03-22 00:46:02 -0600

    Test a negative number.

--- lang/js/test/test_avro.js
+++ lang/js/test/test_avro.js
@@ -5,4 +5,5 @@ assert_equal = function(a, b) {
 }
 var bb = Avro.raw.create_byte_buffer_from_string;
 assert_equal(Avro.raw.decode_long(bb('\x06')), 3);
+assert_equal(Avro.raw.decode_long(bb('\x03')), -2);
 assert_equal(Avro.raw.decode_string(bb('\x06foo')), 'foo')
